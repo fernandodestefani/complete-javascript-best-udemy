0c739b5b0b2964e765654a2b39386223e764af0d
Fernando Destefani <henriqueorei@gmail.com>
2025-04-05 09:22:25 -0300

    updating guess-my-number game

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,10 +10,9 @@ document.querySelector(".score").textContent = 10;
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value); */
 
-const secretNumber = Math.trunc(Math.random()*20) + 1;
+let secretNumber = Math.trunc(Math.random()*20) + 1;
 let score = 20; // state variable
-
-document.querySelector(".number").textContent = secretNumber;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener('click', function() {
   const guess = Number(document.querySelector(".guess").value);
@@ -25,6 +24,7 @@ document.querySelector(".check").addEventListener('click', function() {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = "🎉 Correct Number!";
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     // whenever manipulating a style, always specified a string!
     document.querySelector(".number").style.width = '30rem';
@@ -53,3 +53,15 @@ document.querySelector(".check").addEventListener('click', function() {
   }
 
 });
+
+// Coding Challenge #1
+document.querySelector(".again").addEventListener('click', function() {
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  secretNumber = Math.trunc(Math.random()*20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+})
