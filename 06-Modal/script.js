f49a7f7abdbf2571917fe567740d3b0ed2ec5906
Fernando Destefani <henriqueorei@gmail.com>
2025-04-06 09:48:23 -0300

    modal logic development

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// we use the functionality of adding/removing classes to modify the website elements all the time
+
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
@@ -7,7 +9,23 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 // console.log(btnsOpenModal); 
 
-/* for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
-} */
+const openModal = function() {
+  modal.classList.remove('hidden'); //the dot is only for the selector
+  overlay.classList.remove("hidden");
+}
+
+const closeModal = function() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal); // without (), otherwise the function will be executed regardless the click or not
+
+
 
