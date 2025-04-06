de0fa417ba95f20b9589b60d3ae80d1eff207339
Fernando Destefani <henriqueorei@gmail.com>
2025-04-06 08:36:51 -0300

    modal project

---
+++ 06-Modal/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+// querySelector => only the first element in case of multiple elements gets selected
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+// console.log(btnsOpenModal); 
+
+/* for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+} */
+
