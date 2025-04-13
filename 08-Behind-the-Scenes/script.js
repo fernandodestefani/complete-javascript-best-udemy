f5b47d0bd0c62a1a73616b9fe59474333c839c73
Fernando Destefani <henriqueorei@gmail.com>
2025-04-13 09:17:15 -0300

    a few notes about behind the scenes

---
+++ 08-Behind-the-Scenes/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// global scope, function scope and block scope;
+// functions are block scope if 'use strict' is declarated;
+// javascript always try first to look for the variable name in the current scope;
+// therefore, its not a problem at all to have different functions with similar parameter names
+// its possible and acceptable to have different variables with equal names 
+
+/* function calcAge(birthYear) {
+  const age = 2025 - birthYear;
+  console.log(firstName);
+  return age;
+}
+
+const firstName = "Fernando";
+calcAge(); */
+
