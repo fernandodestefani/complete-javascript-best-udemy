db3ed96e153605def7e4cdbf7a4a633f6b97df69
Fernando Destefani <henriqueorei@gmail.com>
2025-04-19 09:40:28 -0300

    data structures, modern operators and strings

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -29,6 +30,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
 
   openingHours: {
     thu: {
@@ -45,3 +49,44 @@ const restaurant = {
     },
   },
 };
+
+// destructuring is simply breaking complex data structure down to simpler data structure like a variable
+/* const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+ */
+/* const [x, y, z] = arr; // destructuring an array
+console.log(x, y, z);
+console.log(arr); // we are not destroying the array
+ */
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// Switching variables
+// if we want to switch the values
+/* const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary); */
+
+//do not need the temporary value in the middle
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// Receive 2 return values from a function
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// Destructuring inside destructuring
+// const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// Default variables - when we dont know the lenght of the array - useful when getting data from API
+/* const [p=1, q=1, r=1] = [8, 9];
+console.log(p, q, r); */
+
