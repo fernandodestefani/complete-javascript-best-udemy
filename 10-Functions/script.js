9cc8aa2ac2b59ca16635a5bad20dd635d0c010af
Fernando Destefani <henriqueorei@gmail.com>
2025-05-10 17:06:23 -0300

    function - default parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,2 +1,18 @@
 'use strict';
 // Default Parameters
+const bookings = [];
+const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers){ //it can be dinamically calculated by default
+  // numPassengers = numPassengers || 1; old way of doing default parameters
+  
+  const booking = {
+    flightNum,
+    numPassengers,
+    price
+  };
+  console.log(booking);
+  bookings.push(booking);
+}
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', undefined, 1000);
