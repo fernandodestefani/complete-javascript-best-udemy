3ef9816566be2c587deee8c5f5ce33c343879994
Fernando Destefani <155449551+fernandodestefani@users.noreply.github.com>
2025-07-20 14:07:23 -0300

    adding, selecting and removing elements

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -29,3 +29,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// Selecting Elements
+// for only these three, we cannot need to select elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header'); // the first element with such class
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById("section--1"); // we dont need the selector # returns nodeList and dont update itself if an element is deleted
+const allButtons = document.getElementsByTagName('button'); //collection - updates automatically
+console.log(allButtons);
+
+document.getElementsByClassName('btn'); //we dont need the selector and also returns a live html collection
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message')
+// message.textContent = "We use cookies for improved functionality and analytics."
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie" >Got it!</button>'
+
+// each dom element is really unique 
+// header.prepend(message); // add the element as the first child
+header.append(message) // the last child
+// header.append(message.cloneNode(true))
+
+/* header.before(message) // as siblings
+header.after(message) */ 
+
+// Delete elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove();
+})
+
+// Styles
