973ec3e4ff7a898549f550d47e0f3190c5d518ad
Fernando Destefani <155449551+fernandodestefani@users.noreply.github.com>
2025-07-26 09:00:18 -0300

    DOM Traversing

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -210,3 +210,35 @@ document.querySelector(".nav").addEventListener(
 );
  */
 
+// DOM Traversing
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children); //only for direct children
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+// important method, especially for event delegation
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)'
+
+// Going sideways: siblings
+//elements
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+//nodes
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+//all the siblings
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(function(el){
+  if(el !== h1) el.style.transform = 'scale(0.5)'
+})
