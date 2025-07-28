dc5f99bcbe48940cc2161cf1777a715aa08ce23b
Fernando Destefani <155449551+fernandodestefani@users.noreply.github.com>
2025-07-28 20:41:41 -0300

    Building a Slider Component Part 1

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -207,6 +207,52 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(img => imgObserver.observe(img))
 
 ////////////////////////////////////////////////////
+// Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left')
+const btnRight = document.querySelector('.slider__btn--right')
+
+/* const slider = document.querySelector('.slider')
+slider.style.transform = 'scale(0.75)'
+slider.style.overflow = 'visible' */
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+slides.forEach((s, i) => s.style.transform = `translateX(${100 * i}%)`)
+// 0% (active slide), 100%, 200%, 300%...
+
+const goToSlide = function(slide){
+  slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  // curSlide 1: -100%, 0%, 100%, 200%...
+};
+goToSlide(0);
+
+// Next slide
+const nextSlide = function() {
+  if(curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide ++;
+  }
+  goToSlide(curSlide)
+}
+
+const prevSlide = function(){
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide)
+}
+
+btnRight.addEventListener('click', nextSlide)
+btnLeft.addEventListener('click', prevSlide)
+// curSlide 1: -100%, 0%, 100%, 200%...
+
+
+////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 
