19b0622d5c5775ce35e6ee35d0318a8bddeace9d
Fernando Destefani <155449551+fernandodestefani@users.noreply.github.com>
2025-07-30 19:27:54 -0300

    lifecycle DOM events

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -466,3 +466,20 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function(el){
   if(el !== h1) el.style.transform = 'scale(0.5)'
 }) */
+
+/////////////////////////////////////////
+// Lifecycle DOM Events
+document.addEventListener('DOMContentLoaded', function(e){
+  console.log('HTML parsed and DOM tree built!', e);
+}) // thats why we should put the script tag at the end of the HTML
+
+window.addEventListener('load', function(e){
+  console.log('Page fully loaded', e);
+})
+
+/* window.addEventListener('beforeunload', function(e){
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = ''
+}) */
+
