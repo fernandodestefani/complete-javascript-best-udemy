854be8f0ab0da324528f63a8d801a6a894612ede
Fernando Destefani <155449551+fernandodestefani@users.noreply.github.com>
2025-08-04 10:07:47 -0300

    lecture: inheritance between classes: Object.create()

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -321,7 +321,7 @@ console.log(car1); */
 
 ///////////////////////////////////
 // Inheritance between ES6 Classes
-class PersonCl {
+/* class PersonCl {
   constructor(fullName, birthYear){
     this.fullName = fullName;
     this.birthYear = birthYear;
@@ -375,7 +375,33 @@ class StudentCl extends PersonCl {
 
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
-martha.calcAge();
+martha.calcAge(); */
 
 ///////////////////////////////////
 // Inheritance between 'Classes': Object.create()
+const PersonProto = {
+  calcAge() {
+    console.log(2025 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+}
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function(firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear)
+  this.course = course;
+}
+
+StudentProto.introduce = function(){
+  console.log(`My name is ${this.firstName} and I study ${this.course}.`);  
+}
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2006, 'Software Engineering');
+jay.introduce();
+jay.calcAge();
