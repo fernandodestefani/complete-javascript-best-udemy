a7bda2786938bd6889367f0dbe5b78188b8a7f9c
Fernando Destefani <155449551+fernandodestefani@users.noreply.github.com>
2025-08-06 09:54:00 -0300

    lecture: callback hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -15,7 +15,7 @@ const countriesContainer = document.querySelector(".countries");
 ///////////////////////////////////////
 // Our first AJAX Call
 // Old school way
-const getCountryData = function (country) {
+/* const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v2/name/${country}`);
   request.send();
@@ -39,8 +39,60 @@ const getCountryData = function (country) {
     countriesContainer.insertAdjacentHTML("beforeend", html);
     countriesContainer.style.opacity = 1;
   });
-};
-
-getCountryData('canada');
+}; */
+// running in parallel, we cannot control which one is completed first
+/* getCountryData('canada');
 getCountryData('italy');
-getCountryData('brazil');
+getCountryData('brazil'); */
+
+///////////////////////////////////////
+// Wellcome to callback hell
+const renderCountry = function(data, className = "") {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          Number(data.population) / 1000000
+        ).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+      </article>
+  `;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    
+    // Render country 1
+    renderCountry(data);
+    
+    // Get neighbour country
+    const neighbour = data.borders?.[0];
+    // AJAX call neighbour country
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    
+    request2.addEventListener('load', function(){
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour')
+    })
+  });
+};
+// callback hell: nested callbacks in order to execute asynchronous tasks in sequence
+// callback hell makes our code messy and hard to mantain and very difficult to understand and reason about it
+getCountryAndNeighbour('portugal')
