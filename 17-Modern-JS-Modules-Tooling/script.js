4c61050e82bbbb56990f5d3243585ec6926372db
Fernando Destefani <155449551+fernandodestefani@users.noreply.github.com>
2025-08-11 18:55:14 -0300

    lecture: overview of modern javascript development

---
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+////////////////////////////////////
+// overview of modern javascript development
+//npm: both repository and software to install packages. it also contains development tools like parcel 
+//build process: webpack or parcel (0 configuration bundle) - (bundlers)
+// 1 bundling - join all modules into one file;
+// 2 transpiling/polyfiling => convert modern js back to ES5, usually done using a tool called babel
+// javascript bundle ready to be deployed on a server for production
+
+////////////////////////////////////
+// overview of modules in js
